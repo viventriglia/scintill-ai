44ef6f9a461d0a9208bf3afeacf9599234ccc337
Vincenzo <vincenzo.ventriglia@outlook.com>
2024-09-30 16:48:39 +0200

    add function to download solar indices, evaluate h component of the magnetometer data

--- scintill_ai/io.py
+++ scintill_ai/io.py
@@ -1,6 +1,9 @@
 from pathlib import Path
+from io import StringIO
+import requests
 
 import pandas as pd
+import numpy as np
 
 
 def read_iaga_file(file_path: Path, **kwargs) -> pd.DataFrame:
@@ -74,4 +77,71 @@ def get_magnetometer_data(data_path: Path) -> pd.DataFrame:
             else:
                 data[yr_].append(df)
 
-    return pd.concat([pd.concat(df_list) for df_list in data.values()])
+    df_mag = pd.concat([pd.concat(df_list) for df_list in data.values()])
+
+    # The H component is the (square root of the) sum of the squares of X and Y components
+    df_mag["h"] = np.round(
+        np.sqrt(df_mag["x"].pow(2.0) + df_mag["y"].pow(2.0)),
+        2,
+    )
+
+    return df_mag
+
+
+def get_solar_data(start_date: str, end_date: str) -> pd.DataFrame:
+    """
+    Function that downloads solar indices (F10.7, sunspot number) within a specified
+    time interval as collected by GFZ German Research Centre for Geosciences
+
+    Parameters
+    ----------
+    start_date : str
+        Start date of the time interval in 'YYYY-MM-DD' format
+    end_date : str
+        End date of the time interval in 'YYYY-MM-DD' format
+
+    Returns
+    -------
+    pd.DataFrame
+    """
+    response = requests.get(
+        "https://kp.gfz-potsdam.de/app/files/Kp_ap_Ap_SN_F107_since_1932.txt"
+    )
+    if response.status_code != 200:
+        raise Exception(f"Error while downloading data: {response.status_code}")
+
+    data = StringIO(response.text)
+
+    # Skipping the header
+    data_lines = []
+    for line in data:
+        if not line.startswith("#"):
+            data_lines.append(line)
+
+    # Creating a DataFrame out of the .txt file
+    df = pd.read_csv(
+        StringIO("".join(data_lines)),
+        sep="\s+",
+        header=None,
+        usecols=[0, 1, 2, 24, 26],
+        names=[
+            "year",
+            "month",
+            "day",
+            "ssn",
+            "f10.7_adj",
+        ],
+        na_values=[-1.0],
+    )
+
+    df["date"] = pd.to_datetime(
+        df["year"].astype(str)
+        + "-"
+        + df["month"].astype(str)
+        + "-"
+        + df["day"].astype(str)
+    )
+
+    df = df.drop(columns=["year", "month", "day"]).set_index("date")
+
+    return df.loc[start_date:end_date]
