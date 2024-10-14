9c12cf2f840c5ecea80b92adb59eab3ed9819890
Vincenzo <vincenzo.ventriglia@outlook.com>
2024-10-14 19:22:59 +0200

    new coords (Presidente Prudente, Brazil) and new vars

--- scintill_ai/__init__.py
+++ scintill_ai/__init__.py
@@ -1,9 +1,12 @@
 from pathlib import Path
 
-# Solar Zenith Angle
-LATITUDE = 50.110656 #FIXME
-LONGITUDE = 8.682526 #FIXME
+# GNSS (Presidente Prudente)
+LATITUDE = -22.122112
+LONGITUDE = -51.407095
 ALTITUDE = 350_000
+ELEVATION_THRESHOLD = 60
+LW_S4_THRESHOLD = 0.4
+UP_S4_THRESHOLD = 0.7
 
 # Data
 DATA_IN = Path("..", "data", "in")
