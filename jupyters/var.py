b30e6990e44d70b4a0751935bb74a0ecf2d511a9
Vincenzo <vincenzo.ventriglia@outlook.com>
2025-02-04 17:55:20 +0100

    SuperMAG magnetometers data

--- jupyters/var.py
+++ jupyters/var.py
@@ -3,5 +3,8 @@ from pathlib import Path
 DATA_IN = Path("..", "data", "in")
 DATA_OUT = Path("..", "data", "out")
 
-START_DATE = "2014-01-01"
-END_DATE = "2023-12-31"
+START_DATE = "2020-01-01"
+END_DATE = "2024-12-31"
+
+SMAG_YEARS = [2020, 2021, 2022, 2023, 2024]
+SMAG_STATIONS = ['KOU', 'TTB']
