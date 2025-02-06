908a9adc0fc210e1ec2e02706fa80c4987975b18
Vincenzo <vincenzo.ventriglia@outlook.com>
2025-02-06 17:39:13 +0100

    type hint

--- scintill_ai/__init__.py
+++ scintill_ai/__init__.py
@@ -3,13 +3,16 @@ from pathlib import Path
 from dotenv import dotenv_values
 
 # GNSS (Presidente Prudente)
-ISMR_KEY = dotenv_values("../.env.secret")["ISMR_KEY"]
-LATITUDE = -22.122112
-LONGITUDE = -51.407095
-ALTITUDE = 350_000
-ELEVATION_THRESHOLD = 60
-LW_S4_THRESHOLD = 0.4
-UP_S4_THRESHOLD = 0.7
+LATITUDE: float = -22.122112
+LONGITUDE: float = -51.407095
+ALTITUDE: float = 350_000
+ELEVATION_THRESHOLD: float = 60
+LW_S4_THRESHOLD: float = 0.4
+UP_S4_THRESHOLD: float = 0.7
+
+# ISMR
+ISMR_KEY: str = dotenv_values("../.env.secret")["ISMR_KEY"]
+MAX_CONCURRENT_REQUESTS: int = 5
 
 # Data
-DATA_IN = Path("..", "data", "in")
+DATA_IN: Path = Path("..", "data", "in")
