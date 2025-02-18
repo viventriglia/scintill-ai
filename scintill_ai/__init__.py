2d68352d461f723ae788aa64eab259fff5e87c4c
Vincenzo <vincenzo.ventriglia@outlook.com>
2025-02-18 19:59:20 +0100

    WIP exploring conformal methods

--- scintill_ai/__init__.py
+++ scintill_ai/__init__.py
@@ -19,4 +19,4 @@ DATA_IN: Path = Path("..", "data", "in")
 
 # EnbPI & ACI
 ALPHAS = [1 - 0.80, 1 - 0.90, 1 - 0.95]
-GAP = 30
+FCST_HORIZON = 10
--- scintill_ai/conformal.py
+++ scintill_ai/conformal.py
@@ -1,36 +1,36 @@
+from typing import Literal
+
 import numpy as np
+from numpy.typing import NDArray
 import pandas as pd
-from mapie.regression import MapieTimeSeriesRegressor
+from sklearn.base import RegressorMixin
+from mapie.regression import MapieRegressor, MapieTimeSeriesRegressor
+from mapie.subsample import BlockBootstrap
 from mapie.metrics import (
     regression_coverage_score,
     coverage_width_based,
     regression_mean_width_score,
 )
 
-from scintill_ai import ALPHAS, GAP
+from scintill_ai import ALPHAS, FCST_HORIZON
 
 
 def enbpi_ts_regressor_predict(
-    model,
-    cv,
+    model: RegressorMixin,
+    cv: BlockBootstrap,
     train_data: tuple[pd.DataFrame, pd.Series],
     test_data: tuple[pd.DataFrame, pd.Series],
     alpha_list: list[float] = ALPHAS,
 ) -> dict:
 
+    ts_regressor = fit_mapie_regressor(
+        model=model, method="enbpi", cv=cv, train_data=train_data
+    )
+
     results = []
     for alpha in alpha_list:
-        ts_regressor = MapieTimeSeriesRegressor(
-            model,
-            method="enbpi",
-            cv=cv,
-            agg_function="mean",
-            n_jobs=-1,
-        )
-
-        ts_regressor.fit(train_data[0], train_data[1])
         y_pred, y_pis = ts_regressor.predict(
-            test_data[0],
+            test_data[0].to_numpy(),
             alpha=alpha,
             ensemble=True,
         )
@@ -39,15 +39,7 @@ def enbpi_ts_regressor_predict(
             {
                 "y_pred": y_pred,
                 "y_pis": y_pis,
-                "coverage": regression_coverage_score(
-                    test_data[1], y_pis[:, 0, 0], y_pis[:, 1, 0]
-                ),
-                "mean_width": regression_mean_width_score(
-                    y_pis[:, 1, 0], y_pis[:, 0, 0]
-                ),
-                "cwc": coverage_width_based(
-                    test_data[1], y_pis[:, 0, 0], y_pis[:, 1, 0], eta=5, alpha=alpha
-                ),
+                **evaluate_metrics(test_data, y_pis, alpha),
                 "alpha": alpha,
             }
         )
@@ -56,53 +48,50 @@ def enbpi_ts_regressor_predict(
 
 
 def aci_ts_regressor_predict(
-    model,
-    cv,
+    model: RegressorMixin,
+    cv: BlockBootstrap,
     gamma: float,
     train_data: tuple[pd.DataFrame, pd.Series],
     test_data: tuple[pd.DataFrame, pd.Series],
     alpha_list: list[float] = ALPHAS,
-    gap: int = GAP,
+    forecast_horizon: int = FCST_HORIZON,
 ) -> dict:
+
+    ts_regressor = fit_mapie_regressor(
+        model=model, method="aci", cv=cv, train_data=train_data
+    )
 
     results = []
     for alpha in alpha_list:
-        ts_regressor = MapieTimeSeriesRegressor(
-            model,
-            method="aci",
-            cv=cv,
-            agg_function="mean",
-            n_jobs=-1,
-        )
-
-        ts_regressor.fit(train_data[0].to_numpy(), train_data[1].to_numpy())
-
         y_pred = np.zeros(test_data[1].shape)
         y_pis = np.zeros((test_data[1].shape[0], 2, 1))
-        y_pred[:gap], y_pis[:gap, :, :] = ts_regressor.predict(
-            test_data[0].iloc[:gap, :].to_numpy(),
-            alpha=alpha,
-            ensemble=True,
-            allow_infinite_bounds=True,
+        y_pred[:forecast_horizon], y_pis[:forecast_horizon, :, :] = (
+            ts_regressor.predict(
+                test_data[0].iloc[:forecast_horizon, :].to_numpy(),
+                alpha=alpha,
+                ensemble=True,
+                allow_infinite_bounds=True,
+            )
         )
 
-        for step in range(gap, test_data[0].shape[0], gap):
+        for step in range(forecast_horizon, test_data[0].shape[0], forecast_horizon):
             ts_regressor.adapt_conformal_inference(
-                test_data[0].iloc[(step - gap) : step, :].to_numpy(),
-                test_data[1].iloc[(step - gap) : step].to_numpy(),
+                test_data[0].iloc[(step - forecast_horizon) : step, :].to_numpy(),
+                test_data[1].iloc[(step - forecast_horizon) : step].to_numpy(),
                 gamma=gamma,
             )
 
-            y_pred[step : step + gap], y_pis[step : step + gap, :, :] = (
-                ts_regressor.predict(
-                    test_data[0].iloc[step : step + gap, :].to_numpy(),
-                    alpha=alpha,
-                    ensemble=True,
-                    allow_infinite_bounds=True,
-                )
+            (
+                y_pred[step : step + forecast_horizon],
+                y_pis[step : step + forecast_horizon, :, :],
+            ) = ts_regressor.predict(
+                test_data[0].iloc[step : step + forecast_horizon, :].to_numpy(),
+                alpha=alpha,
+                ensemble=True,
+                allow_infinite_bounds=True,
             )
-            y_pis[step : step + gap, :, :] = np.clip(
-                a=y_pis[step : step + gap, :, :],
+            y_pis[step : step + forecast_horizon, :, :] = np.clip(
+                a=y_pis[step : step + forecast_horizon, :, :],
                 a_min=0,
                 a_max=1,
             )
@@ -111,17 +100,38 @@ def aci_ts_regressor_predict(
             {
                 "y_pred": y_pred,
                 "y_pis": y_pis,
-                "coverage": regression_coverage_score(
-                    test_data[1].values, y_pis[:, 0, 0], y_pis[:, 1, 0]
-                ),
-                "mean_width": regression_mean_width_score(
-                    y_pis[:, 1, 0], y_pis[:, 0, 0]
-                ),
-                "cwc": coverage_width_based(
-                    test_data[1], y_pis[:, 0, 0], y_pis[:, 1, 0], eta=5, alpha=alpha
-                ),
+                **evaluate_metrics(test_data, y_pis, alpha),
                 "alpha": alpha,
             }
         )
 
     return results
+
+
+def evaluate_metrics(
+    test_data: tuple[pd.DataFrame, pd.Series], y_pis: NDArray[np.float64], alpha: float
+):
+    return {
+        "coverage": regression_coverage_score(
+            test_data[1].values, y_pis[:, 0, 0], y_pis[:, 1, 0]
+        ),
+        "mean_width": regression_mean_width_score(y_pis[:, 1, 0], y_pis[:, 0, 0]),
+        "cwc": coverage_width_based(
+            test_data[1], y_pis[:, 0, 0], y_pis[:, 1, 0], eta=5, alpha=alpha
+        ),
+    }
+
+
+def fit_mapie_regressor(
+    model: RegressorMixin,
+    method: Literal["enbpi", "aci"],
+    cv: BlockBootstrap,
+    train_data: tuple[pd.DataFrame, pd.Series],
+) -> MapieRegressor:
+    return MapieTimeSeriesRegressor(
+        model,
+        method=method,
+        cv=cv,
+        agg_function="mean",
+        n_jobs=-1,
+    ).fit(train_data[0].to_numpy(), train_data[1].to_numpy())
