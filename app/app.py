45b4c1a9f6e1366e7acb1cd2f09512c3558755ea
Vincenzo <vincenzo.ventriglia@outlook.com>
2025-03-25 16:09:06 +0100

    multi page app, metrics evaluation

--- app/app.py
+++ app/app.py
@@ -1,43 +1,96 @@
 from pathlib import Path
-
 import taipy.gui as tpg
 
 from app.plot import plot_pis
-from app.io import read_model_output
+from app.io import read_model_output, aggregated_evaluation
 from app import DATA_PATH
 
-df_aci = read_model_output(Path(DATA_PATH, "eval_aci.parquet"))
-df_cqr_ct = read_model_output(Path(DATA_PATH, "eval_cqr_ct.parquet"))
-df_aci_5_ahead = read_model_output(Path(DATA_PATH, "eval_aci_5_ahead.parquet"))
-df_cqr_ct_5_ahead = read_model_output(Path(DATA_PATH, "eval_cqr_ct_5_ahead.parquet"))
+df_aci_1 = read_model_output(Path(DATA_PATH, "eval_aci.parquet"))
+df_cqr_1 = read_model_output(Path(DATA_PATH, "eval_cqr_ct.parquet"))
+df_aci_5 = read_model_output(Path(DATA_PATH, "eval_aci_5_ahead.parquet"))
+df_cqr_5 = read_model_output(Path(DATA_PATH, "eval_cqr_ct_5_ahead.parquet"))
 
-fig_aci = plot_pis(df_aci)
-fig_cqr_ct = plot_pis(df_cqr_ct)
-fig_aci_5_ahead = plot_pis(df_aci_5_ahead)
-fig_cqr_ct_5_ahead = plot_pis(df_cqr_ct_5_ahead)
+df_aci_1_wc, df_aci_1_yc = aggregated_evaluation(df_aci_1)
+df_cqr_1_wc, df_cqr_1_yc = aggregated_evaluation(df_cqr_1)
+df_aci_5_wc, df_aci_5_yc = aggregated_evaluation(df_aci_5)
+df_cqr_5_wc, df_cqr_5_yc = aggregated_evaluation(df_cqr_5)
 
-page = """
-# ✨ Scintill-AI
+fig_aci_1 = plot_pis(df_aci_1)
+fig_cqr_1 = plot_pis(df_cqr_1)
+fig_aci_5 = plot_pis(df_aci_5)
+fig_cqr_5 = plot_pis(df_cqr_5)
 
-## ⏱️ 1 minute ahead
+chart_1min = """
+## CatBoost + Adaptive Conformal Inference (ACI)
+<|chart|figure={fig_aci_1}|>
 
-### CatBoost + Adaptive Conformal Inference (ACI)
-<|chart|figure={fig_aci}|>
-
-### CatBoost + Conformalised Quantile Regression (CQR)
-<|chart|figure={fig_cqr_ct}|>
-
-## ⏱️ 5 minutes ahead
-
-### CatBoost + Adaptive Conformal Inference (ACI)
-<|chart|figure={fig_aci_5_ahead}|>
-
-### CatBoost + Conformalised Quantile Regression (CQR)
-<|chart|figure={fig_cqr_ct_5_ahead}|>
+## CatBoost + Conformalised Quantile Regression (CQR)
+<|chart|figure={fig_cqr_1}|>
 """
 
+chart_5min = """
+## CatBoost + Adaptive Conformal Inference (ACI)
+<|chart|figure={fig_aci_5}|>
+
+## CatBoost + Conformalised Quantile Regression (CQR)
+<|chart|figure={fig_cqr_5}|>
+"""
+
+metric_1min = """
+## CatBoost + Adaptive Conformal Inference (ACI)
+
+<|Aggregated by width|expandable|expanded=False|
+<|{df_aci_1_wc}|table|show_all|width=100%|>
+|>
+
+<|Aggregated by <S4>|expandable|expanded=False|
+<|{df_aci_1_yc}|table|show_all|width=100%|>
+|>
+
+## CatBoost + Conformalised Quantile Regression (CQR)
+
+<|Aggregated by width|expandable|expanded=False|
+<|{df_cqr_1_wc}|table|show_all|width=100%|>
+|>
+
+<|Aggregated by <S4>|expandable|expanded=False|
+<|{df_cqr_1_yc}|table|show_all|width=100%|>
+|>
+"""
+
+metric_5min = """
+## CatBoost + Adaptive Conformal Inference (ACI)
+
+<|Aggregated by width|expandable|expanded=False|
+<|{df_aci_5_wc}|table|show_all|width=100%|>
+|>
+
+<|Aggregated by <S4>|expandable|expanded=False|
+<|{df_aci_5_yc}|table|show_all|width=100%|>
+|>
+
+## CatBoost + Conformalised Quantile Regression (CQR)
+
+<|Aggregated by width|expandable|expanded=False|
+<|{df_cqr_5_wc}|table|show_all|width=100%|>
+|>
+
+<|Aggregated by <S4>|expandable|expanded=False|
+<|{df_cqr_5_yc}|table|show_all|width=100%|>
+|>
+"""
+
+pages = {
+    "/": """<|navbar|lov={[("/1-min-ahead-chart", "1-min ahead 📈"), ("/1-min-ahead-metric", "1-min ahead 🔎"), ("/5-min-ahead-chart", "5-min ahead 📈"), ("/5-min-ahead-metric", "5-min ahead 🔎")]}|>""",
+    "1-min-ahead-chart": chart_1min,
+    "1-min-ahead-metric": metric_1min,
+    "5-min-ahead-chart": chart_5min,
+    "5-min-ahead-metric": metric_5min,
+}
+
 if __name__ == "__main__":
-    tpg.Gui(page).run(
+    gui = tpg.Gui(pages=pages)
+    gui.run(
         title="Scintill-AI",
         dark_mode=True,
         use_reloader=True,
--- app/io.py
+++ app/io.py
@@ -2,6 +2,8 @@ from pathlib import Path
 
 import pandas as pd
 
+N_CUT = 9
+
 
 def read_model_output(path: Path) -> pd.DataFrame:
     df = (
@@ -14,3 +16,36 @@ def read_model_output(path: Path) -> pd.DataFrame:
         "y_pred_low"
     ].clip(0)
     return df
+
+
+def aggregated_evaluation(
+    df: pd.DataFrame, n_cut: int = N_CUT
+) -> tuple[pd.DataFrame, pd.DataFrame]:
+    df_ = df.copy()
+    df_["width"] = df_["y_pred_hig"] - df_["y_pred_low"]
+    df_["width_category"] = pd.cut(df_["width"], bins=n_cut)
+    df_["y_test_category"] = pd.cut(df_["y_test"], bins=n_cut)
+
+    # Aggregate by width size
+    df_agg_wc = df_.groupby("width_category", observed=False, as_index=False).agg(
+        n_samples=("width", "count"),
+        n_covered=("is_covered", "sum"),
+        mean_y_test=("y_test", "mean"),
+    )
+    df_agg_wc["perc_covered"] = (
+        100 * df_agg_wc["n_covered"].div(df_agg_wc["n_samples"])
+    ).round(1)
+    df_agg_wc["width_category"] = df_agg_wc["width_category"].astype(str)
+
+    # Aggregate by <S4>
+    df_agg_yc = df_.groupby("y_test_category", observed=False, as_index=False).agg(
+        n_samples=("width", "count"),
+        n_covered=("is_covered", "sum"),
+        mean_width=("width", "mean"),
+    )
+    df_agg_yc["perc_covered"] = (
+        100 * df_agg_yc["n_covered"].div(df_agg_yc["n_samples"])
+    ).round(1)
+    df_agg_yc["y_test_category"] = df_agg_yc["y_test_category"].astype(str)
+
+    return df_agg_wc, df_agg_yc
